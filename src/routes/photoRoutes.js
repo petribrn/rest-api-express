3a0da363083748f70ffe22e638495991e706666f
petribrn <brunoopetri@gmail.com>
2022-08-17 17:42:47 -0300

    file ext treatment

--- src/routes/photoRoutes.js
+++ src/routes/photoRoutes.js
@@ -1,11 +1,8 @@
 import { Router } from 'express';
-import multer from 'multer';
 import photoController from '../controllers/PhotoController';
-import multerConfig from '../config/multer';
 
 const router = new Router();
-const upload = multer(multerConfig);
 
-router.post('/', upload.single('photo'), photoController.index);
+router.post('/', photoController.index);
 
 export default router;
