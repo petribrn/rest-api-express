f3c88630cecd60e7b58a53561b49dfe53eee81f9
petribrn <brunoopetri@gmail.com>
2022-08-23 16:28:21 -0300

    dist update

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,5 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('express'); var _express2 = _interopRequireDefault(_express);
-var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
+// import helmet from 'helmet';
 var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
 var _path = require('path');
 var _corsConfig = require('./config/corsConfig'); var _corsConfig2 = _interopRequireDefault(_corsConfig);
@@ -21,7 +21,7 @@ class App {
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
     this.app.use('/images/', _express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads', 'images')));
-    this.app.use(_helmet2.default.call(void 0, ));
+    // this.app.use(helmet());
     this.app.use(_cors2.default.call(void 0, _corsConfig2.default));
   }
 
